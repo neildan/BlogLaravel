3194010e6a71f98260b7635db9e74aaf101f06dc
Daniel Felipe Luis Valencia <felipeluiz152000@gmail.com>
2020-04-24 22:17:56 -0500

    Se avanza con configuracion de DB, migraciones, rutas, controladores y formularios

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
+// Ejemplo ruta básica
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ejemplo ruta que referencia a otra ruta mediante el nombre
+Route::get('hola/{name}', function ($name) {
+    return "<h1>Hola $name, sobre <a href='".route("postpc")."'>nosotros</a></h1>";
+});
+
+// Ejemplo de ruta con nombre
+Route::get('sobre-nosotros', function () {
+    return "<h1>Toda la información sobre nosotros</h1>";
+})->name("nosotros");
+
+// Ejemplo de ruta de con nombre e implementación view
+Route::get('home/{nombre?}/{apellido?}', function($nombre = 'Daniel', $apellido = 'Valencia') {
+    return view('home', ['nombre' => $nombre, 'apellido' => $apellido]);
+})->name("home");
+
+// Ejemplo de ruta utilizando controlador
+// Route::get('post', 'PostController@index')->name("postpc");
+
+// Ejemplo de ruta resource
+Route::resource('dashboard/post', 'dashboard\PostController');
